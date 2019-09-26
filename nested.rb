485a165a436d9a528abde3405c480633dedf9a36
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-26 18:14:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-programmer_hash[:alan_kay]["changing_alan"][:known_for] = "GUI"#change what Alan Kay is :known_for to the value of the alans_new_info variable.
+programmer_hash[:changing_alan]#change what Alan Kay is :known_for to the value of the alans_new_info variable.
 
  
 end
